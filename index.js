6e91e7f344691df51ba9a7cace91f5a3e420cdf1
Dương Trọng Phúc <dtrongphucdv@gmail.com>
2021-08-19 14:42:58 +0700

    Roman to Integer

--- index.js
+++ index.js
@@ -524,3 +524,98 @@ var intToRoman = function (num) {
 
 	return s;
 };
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function (s) {
+	let map = {
+		I: 1,
+		IV: 4,
+		V: 5,
+		IX: 9,
+		X: 10,
+		XL: 40,
+		L: 50,
+		XC: 90,
+		C: 100,
+		CD: 400,
+		D: 500,
+		CM: 900,
+		M: 1000,
+	};
+	let i = 0,
+		num = 0,
+		n;
+
+	while (i < s.length) {
+		if (i + 2 < s.length && map[s.slice(i, i + 3)]) {
+			n = 3;
+			num += map[s.slice(i, i + 3)];
+		} else if (i + 1 < s.length && map[s.slice(i, i + 2)]) {
+			n = 2;
+			num += map[s.slice(i, i + 2)];
+		} else {
+			n = 1;
+			num += map[s[i]];
+		}
+
+		i += n;
+	}
+
+	return num;
+};
+
+//13. Roman to Integer
+/*
+Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+
+Symbol       Value
+I             1
+V             5
+X             10
+L             50
+C             100
+D             500
+M             1000
+For example, 2 is written as II in Roman numeral, just two one's added together. 12 is written as XII, which is simply X + II. The number 27 is written as XXVII, which is XX + V + II.
+
+Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
+
+I can be placed before V (5) and X (10) to make 4 and 9. 
+X can be placed before L (50) and C (100) to make 40 and 90. 
+C can be placed before D (500) and M (1000) to make 400 and 900.
+Given a roman numeral, convert it to an integer.
+
+1 <= s.length <= 15
+s contains only the characters ('I', 'V', 'X', 'L', 'C', 'D', 'M').
+It is guaranteed that s is a valid roman numeral in the range [1, 3999].
+
+*/
+// 164 ms, faster than 26.87%
+// 44.6 MB, less than 64.47%
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function (s) {
+	let map = {
+		I: 1,
+		V: 5,
+		X: 10,
+		L: 50,
+		C: 100,
+		D: 500,
+		M: 1000,
+	};
+	let i,
+		num = 0,
+		value;
+	for (i = 0; i < s.length; i++) {
+		value = map[s[i]] < map[s[i + 1]] ? -map[s[i]] : map[s[i]];
+		num += value;
+	}
+
+	return num;
+};
