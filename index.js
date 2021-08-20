ff97e0643737b273f063438a1d531ceec2a9c9f1
Dương Trọng Phúc <dtrongphucdv@gmail.com>
2021-08-20 09:56:08 +0700

    3Sum Closest

--- index.js
+++ index.js
@@ -706,3 +706,13 @@ var threeSum = function (nums) {
 
 	return arr;
 };
+
+//16. 3Sum Closest
+/* Given an integer array nums of length n and an integer target, find three integers in nums such that the sum is closest to target.
+
+Return the sum of the three integers.
+
+You may assume that each input would have exactly one solution. */
+/* 3 <= nums.length <= 1000
+-1000 <= nums[i] <= 1000
+-104 <= target <= 104 */
