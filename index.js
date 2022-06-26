7272d915fce4d9f38699e677bd857ddc7d691688
Duong Trong Phuc <dtrongphucdv@gmail.com>
2022-06-26 15:17:40 +0700

    24. Swap Nodes in Pairs

--- index.js
+++ index.js
@@ -983,3 +983,33 @@ var generateParenthesis = function (n) {
 
 	return output;
 };
+
+//24. Swap Nodes in Pairs
+/* Given a linked list, swap every two adjacent nodes and return its head. You must solve the problem without modifying the values in the list's nodes (i.e., only nodes themselves may be changed.)*/
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var swapPairs = function(head) {
+    if(!head || !head.next) return head;
+    const dummyNode = new ListNode();
+    let prevNode = dummyNode;
+    let currNode = head;
+    while(currNode && currNode.next) {
+        prevNode.next = currNode.next;
+        currNode.next = prevNode.next.next;
+        prevNode.next.next = currNode;
+        
+        prevNode = currNode;
+        currNode = currNode.next;
+    }
+    
+    return dummyNode.next;
+};
